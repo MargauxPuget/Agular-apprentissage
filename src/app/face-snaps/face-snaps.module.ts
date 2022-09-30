ea92140adbe5d10f36ab9d44c279013cc203eb58
MargauxPuget <margaux.puget@gmail.com>
2022-09-30 11:38:44 +0200

    lazy loading DONE

--- src/app/face-snaps/face-snaps.module.ts
+++ src/app/face-snaps/face-snaps.module.ts
@@ -4,8 +4,8 @@ import { FaceSnapComponent } from './components/face-snap/face-snap.component';
 import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
 import { SingleFaceSnapComponent } from './components/single-face-snap/single-face-snap.component';
 import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FaceSnapRoutingModule } from './face-snap-routing.modules';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     ReactiveFormsModule,
     // pour que les liens avec changement de route fonction
-    RouterModule
+    FaceSnapRoutingModule
   ],
   exports: [
     FaceSnapComponent,
