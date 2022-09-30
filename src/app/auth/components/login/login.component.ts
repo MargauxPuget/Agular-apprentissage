6044ccd083ae60218c8fd912b131eddd9149dfab
MargauxPuget <margaux.puget@gmail.com>
2022-09-30 12:25:35 +0200

    implémentation de LoginComponent

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onLogin(): void{
+    this.auth.login();
+    // pour une application sercurisé, il faut vérifier que la connexion c'est bien passé !!!
+    this.router.navigateByUrl('/facesnaps');
+  }
 }
